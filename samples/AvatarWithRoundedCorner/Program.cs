b93ea38197100ceb28fb5bb359233d5598d5b679
Scott Williams <tocsoft@gmail.com>
2017-06-21 21:07:05 +0100

    fix generated files

--- samples/AvatarWithRoundedCorner/Program.cs
+++ samples/AvatarWithRoundedCorner/Program.cs
@@ -4,7 +4,9 @@ namespace AvatarWithRoundedCorner
 {
     using System;
     using System.Numerics;
-    using ImageSharp;
+    using SixLabors.ImageSharp;
+    using SixLabors.ImageSharp.PixelFormats;
+    using SixLabors.ImageSharp.Processing;
     using SixLabors.Primitives;
     using SixLabors.Shapes;
 
@@ -23,10 +25,10 @@ namespace AvatarWithRoundedCorner
         {
             using (var image = Image.Load(source))
             {
-                image.Resize(new ImageSharp.Processing.ResizeOptions
+                image.Resize(new SixLabors.ImageSharp.Processing.ResizeOptions
                 {
                     Size = size,
-                    Mode = ImageSharp.Processing.ResizeMode.Crop
+                    Mode = ResizeMode.Crop
                 });
 
                 ApplyRoundedCourners(image, cornerRadius);
@@ -40,17 +42,17 @@ namespace AvatarWithRoundedCorner
             // now we have our corners time to draw them
             img.Fill(Rgba32.Transparent, corners, new GraphicsOptions(true)
             {
-                BlenderMode = ImageSharp.PixelFormats.PixelBlenderMode.Src // enforces that any part of this shape that has color is punched out of the background
+                BlenderMode = PixelBlenderMode.Src // enforces that any part of this shape that has color is punched out of the background
             });
         }
 
         public static  IPathCollection BuildCorners(int imageWidth, int imageHeight, float cornerRadius)
         {
             // first create a square
-            var rect = new SixLabors.Shapes.RectangularePolygon(-0.5f, -0.5f, cornerRadius, cornerRadius);
+            var rect = new RectangularePolygon(-0.5f, -0.5f, cornerRadius, cornerRadius);
 
             // then cut out of the square a circle so we are left with a corner
-            var cornerToptLeft = rect.Clip(new SixLabors.Shapes.EllipsePolygon(cornerRadius-0.5f, cornerRadius - 0.5f, cornerRadius));
+            var cornerToptLeft = rect.Clip(new EllipsePolygon(cornerRadius-0.5f, cornerRadius - 0.5f, cornerRadius));
 
             // corner is now a corner shape positions top left
             //lets make 3 more positioned correctly, we cando that by translating the orgional artound the center of the image
